edcb52a8ab14f935710d2317ac1ce5ba617bf889
Sang Cu <sangcn@gmail.com>
2015-03-03 19:19:09 +0700

    init project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+    /* This is basic - uses default settings */
+
+
+        $("a.single_image").fancybox();
+
+});
